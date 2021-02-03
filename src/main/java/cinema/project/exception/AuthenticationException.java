c37518c6b2a227c8256a6b57814a0d942fe538ef
Yaroslav <yaroslav8oroboro@gmail.com>
2021-02-03 10:15:21 +0200

    Changed type of authentication exception

--- src/main/java/cinema/project/exception/AuthenticationException.java
+++ src/main/java/cinema/project/exception/AuthenticationException.java
@@ -1,6 +1,6 @@
 package cinema.project.exception;
 
-public class AuthenticationException extends RuntimeException {
+public class AuthenticationException extends Exception {
     public AuthenticationException(String message) {
         super(message);
     }
