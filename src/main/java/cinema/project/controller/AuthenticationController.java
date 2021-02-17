7777ea3b6c2535192c000adae2861a09ba9acb40
Yaroslav <yaroslav8oroboro@gmail.com>
2021-02-17 13:11:32 +0200

    Removed unused import

--- src/main/java/cinema/project/controller/AuthenticationController.java
+++ src/main/java/cinema/project/controller/AuthenticationController.java
@@ -2,7 +2,6 @@ package cinema.project.controller;
 
 import cinema.project.model.dto.request.UserRequestDto;
 import cinema.project.service.AuthenticationService;
-import cinema.project.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
