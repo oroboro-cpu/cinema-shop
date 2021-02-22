a9f9735430a4a0a8fa7d46ef8e03970bcd91594e
Yaroslav <yaroslav8oroboro@gmail.com>
2021-02-22 15:38:34 +0200

    added corrections in the loadUserNyUsername method

--- src/main/java/cinema/project/security/UserDetailsServiceImpl.java
+++ src/main/java/cinema/project/security/UserDetailsServiceImpl.java
@@ -1,8 +1,10 @@
 package cinema.project.security;
 
+import static org.springframework.security.core.userdetails.User.UserBuilder;
+import static org.springframework.security.core.userdetails.User.withUsername;
+
 import cinema.project.model.User;
 import cinema.project.service.UserService;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,9 +20,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userService.findByEmail(email).get();
-        UserBuilder userBuilder = org.springframework.security.core.userdetails.User
-                .withUsername(email);
+        User user = userService.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("Can't find user by email: " + email));
+        UserBuilder userBuilder = withUsername(email);
         userBuilder.username(user.getEmail());
         userBuilder.password(user.getPassword());
         userBuilder.authorities(user.getRoles()
